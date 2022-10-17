a804351a550e33e9a8d3a1704e0ddf5a0504f041
Mohamad abdUlaziz <mohamad.samer.abdulaziz@gmail.com>
2022-10-17 08:07:53 +0300

    refactoring activity to launch android 12

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    //to launch android 12 we add
+    //android:exported:'true' to each activity in application and provider receiver
+    // we find the when open manifest in android editor in merge manifest section
+    // this link is helper https://stackoverflow.com/questions/68554294/androidexported-needs-to-be-explicitly-specified-for-activity-apps-targeting
+
     // convert app to english to fit with folioreader not work with arabic device
     // go to vocsy_epub_viewer plugin in android folder to set config channel add this code below to fit with arabic devices
     // Resources res = context.getResources();
@@ -45,148 +51,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-//
-// import 'dart:convert';
-// import 'dart:io';
-//
-// import 'package:dio/dio.dart';
-// import 'package:flutter/material.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'package:vocsy_epub_viewer/epub_viewer.dart';
-// // import 'package:permission_handler/permission_handler.dart';
-//
-// void main() async {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   bool loading = false;
-//   Dio dio = new Dio();
-//   String filePath = "assets/4.epub";
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // download();
-//   }
-//
-//   download() async {
-//     if (Platform.isAndroid || Platform.isIOS) {
-//       print('download');
-//       await downloadFile();
-//     } else {
-//       loading = false;
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Plugin example app'),
-//         ),
-//         body: Center(
-//           child: loading
-//               ? CircularProgressIndicator()
-//               : FlatButton(
-//                   onPressed: () async {
-//                     Directory appDocDir =
-//                         await getApplicationDocumentsDirectory();
-//                     print('$appDocDir');
-//
-//                     String iosBookPath = '${appDocDir.path}/chair.epub';
-//                     print(iosBookPath);
-//                     String androidBookPath = 'file:///android_asset/3.epub';
-//                     EpubViewer.setConfig(
-//                       themeColor: Colors.red,
-//                       // identifier: "iosBook",
-//                       scrollDirection: EpubScrollDirection.ALLDIRECTIONS,
-//                       allowSharing: true,
-//                       nightMode: true,
-//                     );
-//
-//                     // get current locator
-//                     EpubViewer.locatorStream.listen((locator) {
-//                       print(
-//                           'LOCATOR: ${EpubLocator.fromJson(jsonDecode(locator))}');
-//                     });
-//
-//                     EpubViewer.openAsset(
-//                       filePath,
-//
-//                     );
-//
-//                     // await EpubViewer.openAsset(
-//                     //   'assets/4.epub',
-//                     //   lastLocation: EpubLocator.fromJson({
-//                     //     "bookId": "2239",
-//                     //     "href": "/OEBPS/ch06.xhtml",
-//                     //     "created": 1539934158390,
-//                     //     "locations": {
-//                     //       "cfi": "epubcfi(/0!/4/4[simple_book]/2/2/6)"
-//                     //     }
-//                     //   }),
-//                     // );
-//                   },
-//                   child: Container(
-//                     child: Text('open epub'),
-//                   ),
-//                 ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Future downloadFile() async {
-//     print('download1');
-//     await startDownload();
-//     // if (await Permission.storage.isGranted) {
-//     //   await Permission.storage.request();
-//     //   await startDownload();
-//     // } else {
-//     //   await startDownload();
-//     // }
-//   }
-//
-//   startDownload() async {
-//     Directory? appDocDir = Platform.isAndroid
-//         ? await getExternalStorageDirectory()
-//         : await getApplicationDocumentsDirectory();
-//
-//     String path = appDocDir!.path + '/chair.epub';
-//     File file = File(path);
-// //    await file.delete();
-//
-//     if (!File(path).existsSync()) {
-//       await file.create();
-//       await dio.download(
-//         'https://github.com/FolioReader/FolioReaderKit/raw/master/Example/'
-//         'Shared/Sample%20eBooks/The%20Silver%20Chair.epub',
-//         path,
-//         deleteOnError: true,
-//         onReceiveProgress: (receivedBytes, totalBytes) {
-//           print((receivedBytes / totalBytes * 100).toStringAsFixed(0));
-//           //Check if download is complete and close the alert dialog
-//           if (receivedBytes == totalBytes) {
-//             loading = false;
-//             setState(() {
-//               //   filePath = path;
-//             });
-//           }
-//         },
-//       );
-//     } else {
-//       loading = false;
-//       setState(() {
-//         //    filePath = path;
-//       });
-//     }
-//   }
-// }
